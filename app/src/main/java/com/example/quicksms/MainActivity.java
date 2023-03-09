61cdfe11cf97d9c0412cb11931ddd4f4dc0617d7
Md. Jubayer <mdjubayer247@gmail.com>
2023-03-10 05:54:52 +0600

    google signin

--- app/src/main/java/com/example/quicksms/MainActivity.java
+++ app/src/main/java/com/example/quicksms/MainActivity.java
@@ -16,6 +16,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
@@ -31,12 +32,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.auth.SignInMethodQueryResult;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,12 +55,18 @@ public class MainActivity extends AppCompatActivity {
     // code to post/handler request for permission
     public final static int REQUEST_CODE = -1010101;
 
-    TextInputEditText emailField,passwordField,repeatPasswordField;
+    TextInputEditText emailField,passwordField,repeatPasswordField,phoneInputFiled, verificationCodeFiled;
+    TextView text_email_phone_option;
     Button continueButton;
     SignInButton googleSigninBtn;
 
 
-    TextInputLayout password_input_field_layout,repeat_password_input_field_layout;
+    TextInputLayout password_input_field_layout,
+            repeat_password_input_field_layout,
+                    email_input_layout,
+                    phone_input_layout,
+            verification_input_layout
+                ;
 
     boolean userExits =false;
     boolean userSigningUp =false;
@@ -59,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     GoogleSignInOptions gso;
     private static final int RC_SIGN_IN = 9001;
+    private static final int SL_OPTION_PHONE = 1111;
+    private static final int SL_OPTION_EMAIL = 3333;
+
+    private int selectedOption =SL_OPTION_EMAIL;
+
+    private boolean onCodeSent=false;
+    private String mVerificationId;
+
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id_s))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
@@ -78,10 +103,41 @@ public class MainActivity extends AppCompatActivity {
         googleSigninBtn =findViewById(R.id.sign_in_button);
         password_input_field_layout = findViewById(R.id.password_input_field_layout);
         repeat_password_input_field_layout = findViewById(R.id.repeat_password_input_field_layout);
+        email_input_layout =findViewById(R.id.email_input_layout);
+        phone_input_layout = findViewById(R.id.phone_input_layout);
+        text_email_phone_option =findViewById(R.id.text_email_phone_option);
+        phoneInputFiled  = findViewById(R.id.phone_input_field);
+        verification_input_layout = findViewById(R.id.verification_input_layout);
+        verificationCodeFiled = findViewById(R.id.verification_input_field);
+        text_email_phone_option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedOption==SL_OPTION_PHONE){
+                    text_email_phone_option.setText("Use phone instead");
+                    email_input_layout.setVisibility(View.VISIBLE);
+                    password_input_field_layout.setVisibility(View.GONE);
+                    selectedOption =SL_OPTION_EMAIL;
+                } else{
+                    selectedOption =SL_OPTION_PHONE;
+                    text_email_phone_option.setText("Use email instead");
+                    email_input_layout.setVisibility(View.GONE);
+                    password_input_field_layout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!userSigningUp && !userSigningIn) {
+                if (selectedOption == SL_OPTION_PHONE && onCodeSent){
+                    String verificationCode = String.valueOf(verificationCodeFiled.getText());
+                    verifyPhoneNumberWithCode(mVerificationId,verificationCode);
+                }
+                else if(selectedOption==SL_OPTION_PHONE){
+                    String phone_number = String.valueOf(phoneInputFiled.getText());
+                    startPhoneNumberVerification(phone_number);
+                }
+
+                else if(!userSigningUp && !userSigningIn) {
                     checkUser();
                     Toast.makeText(getApplicationContext(),"===>checking user",Toast.LENGTH_LONG).show();
                 } else if(userSigningUp){
@@ -124,6 +180,9 @@ public class MainActivity extends AppCompatActivity {
                 signIn();
             }
         });
+
+
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -137,6 +196,54 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }).start();
+
+        // Initialize phone auth callbacks
+        // [START phone_auth_callbacks]
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+                password_input_field_layout.setEnabled(false);
+                verification_input_layout.setVisibility(View.VISIBLE);
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+            }
+        };
+        // [END phone_auth_callbacks]
     }
 
 
@@ -358,6 +465,67 @@ public class MainActivity extends AppCompatActivity {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+
+
+    // [START resend_verification]
+    private void resendVerificationCode(String phoneNumber,
+                                        PhoneAuthProvider.ForceResendingToken token) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .setForceResendingToken(token)     // ForceResendingToken from callbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+    // [END resend_verification]
+
+    // [START sign_in_with_phone]
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+    // [END sign_in_with_phone]
+
+    private void startPhoneNumberVerification(String phoneNumber) {
+        // [START start_phone_auth]
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+        // [END start_phone_auth]
+    }
+
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        // [START verify_with_code]
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        // [END verify_with_code]
+    }
+
 }
 
 
