e27d215f9673cee9197c345f5b4a10c4befc8940
Md. Jubayer <mdjubayer247@gmail.com>
2023-03-10 14:45:07 +0600

    home_screen

--- app/src/main/java/com/example/quicksms/UserHome.java
+++ app/src/main/java/com/example/quicksms/UserHome.java
@@ -5,11 +5,13 @@ package com.example.quicksms;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.helper.widget.MotionEffect;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -31,6 +33,7 @@ public class UserHome extends AppCompatActivity {
     Button btnSave ;
     ProjectUtils projectUtils;
     private DatabaseReference mDatabase;
+    private ImageButton logoutBtn;
     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +51,15 @@ public class UserHome extends AppCompatActivity {
         phone_input_layout = findViewById(R.id.phone_input_layout);
         phoneInputFiled  = findViewById(R.id.phone_input_field);
         btnSave = findViewById(R.id.btn_save);
+        logoutBtn = (ImageButton) findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent loginActivity =new Intent(UserHome.this,MainActivity.class);
+                startActivity(loginActivity);
+            }
+        });
         emailField.setText(email);
         phoneInputFiled.setText(phone);
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -61,8 +73,8 @@ public class UserHome extends AppCompatActivity {
             }
         });
 
-//        projectUtils = new ProjectUtils(this);
-//        projectUtils.checkAllPermission();
+        projectUtils = new ProjectUtils(this);
+        projectUtils.checkAllPermission();
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         System.out.println();
